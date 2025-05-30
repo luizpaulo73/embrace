7b06a3fddcc3bbc5fe217a1bdf45d273c43e1c7f
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-05-30 10:00:13 -0300

    feat: add ProgressBar component with styling and layout

---
+++ components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function ProgressBar() {
+  return (
+    <View style={styles.container}>
+        <View style={[styles.progressBar, { backgroundColor: '#65645F', marginTop: 5, }]}>
+            <View style={[styles.progressBar, { backgroundColor: '#00BAFF', width: '48%' }]} />
+        </View>
+        <View style={{ marginTop: 5, flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+            <Text style={styles.infoProgress}>24/50 Voluntários</Text>
+            <Text style={styles.infoProgress}>48%</Text>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+    progressBar: {
+        height: 10,
+        width: '100%',
+        borderRadius: 9999,
+    },
+    infoProgress: {
+        color: '#ffffff',
+        marginLeft: 5,
+        fontSize: 12,
+    }
+});
