0273622defde893f8412cd49ba9f599c9bad9bc3
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-06-07 20:32:02 -0300

    feat: update index component layout with logo and styled button

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
 import { useRouter } from "expo-router"
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native"
+import { View, TouchableOpacity, Text, StyleSheet, Image } from "react-native"
 
 export default function index() {
 
     const navigate = useRouter();
     return (
         <View style={styles.container}>
-            <TouchableOpacity onPress={() => navigate.push("/feed")}><Text>asdasdasd</Text></TouchableOpacity>
+            <Image source={require('../assets/logos/logo_embrace.png')} style={styles.logo} />
+            <TouchableOpacity style={styles.btnStart} onPress={() => navigate.push("/feed")}>
+                <Text style={styles.textBtn}>Entrar</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -17,5 +20,23 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#262624"
+    },
+    logo: {
+        width: 200,
+        height: 200,
+        marginBottom: 20,
+        borderRadius: 16,
+    },
+    btnStart: {
+        backgroundColor: "#00BAFF",
+        padding: 10,
+        borderRadius: 8,
+        textAlign: "center",
+        alignItems: "center",
+    },
+    textBtn: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: "bold",
     }
 })
