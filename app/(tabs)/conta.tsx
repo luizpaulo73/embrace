156e1aff4e34bc25a7893c6d24c5edc381c85bb3
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-06-08 13:41:35 -0300

    feat: enhance Conta component with user information display and logout functionality

--- app/(tabs)/conta.tsx
+++ app/(tabs)/conta.tsx
@@ -1,4 +1,4 @@
-import { router } from "expo-router"
+import { Link, router } from "expo-router"
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
 import BaseScreen from "../../components/BaseScreen/BaseScreen"
 import { useState, useEffect } from "react"
@@ -28,11 +28,19 @@ export default function Conta() {
     return (  
         <BaseScreen platform="embrace">
             <View style={styles.container}>
-                <View style={{ marginBottom: 10, flexDirection: "row", justifyContent: "space-between", width: "100%" }}> 
-                    <Text style={{ color: "#fff", fontSize: 20, fontWeight: "bold" }}>Conta</Text>
+                <View style={{ marginBottom: 10, flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
+                    <View>
+                        <Text style={styles.name}>João Silva</Text>
+                        <Text style={styles.role}>Voluntário</Text>
+                    </View> 
                     <TouchableOpacity onPress={handleLogout}>
-                        <Text style={{ color: "#fff", fontSize: 16 }}>Sair</Text>
+                        <Text style={styles.btnLogout}>Sair</Text>
                     </TouchableOpacity>   
+                </View>
+                <Text style={styles.infoContaTitle}>Informações da Conta</Text>
+                <View style={styles.infoContaContainer}>
+                    <Text style={styles.infoConta}>Email: qwerty@gmail.com</Text>
+                    <Text style={styles.infoConta}>Telefone: (11)91234-5678</Text>
                 </View>
             </View>
         </BaseScreen>
@@ -52,4 +60,42 @@ const styles = StyleSheet.create({
         marginTop: 10,
         padding: 10,
     },
+    btnLogout: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+        backgroundColor: "#FF0000",
+        padding: 5,
+        borderRadius: 8,
+    },
+    name: {
+        color: "#FFFFFF",
+        fontSize: 24,
+        textAlign: "left",
+        alignSelf: "center",
+        fontWeight: "bold",
+    },
+    role: {
+        color: "#C2C0B6",
+        marginLeft: 5,
+    },
+    infoContaTitle: {
+        color: "#ffffff",
+        fontSize: 20,
+        fontWeight: "bold",
+        marginTop: 20
+    },
+    infoContaContainer: {
+        width: "100%",
+        justifyContent: "space-between",
+        marginBottom: 10,
+        gap: 10,
+        marginTop: 20
+    },
+    infoConta: {
+        color: "#C2C0B6",
+        width: "100%",
+        textAlign: "left",
+        fontSize: 15
+    }
 });
